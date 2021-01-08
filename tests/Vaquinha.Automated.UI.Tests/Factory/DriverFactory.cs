2605396ee03f61d6db032ca10c6e865ecd600789
Ewerton Lima <ehzcl@cin.ufpe.br>
2021-01-08 17:43:02 -0300

    doacao com dados validos e doacao com dados invalidos

--- tests/Vaquinha.Automated.UI.Tests/Factory/DriverFactory.cs
+++ tests/Vaquinha.Automated.UI.Tests/Factory/DriverFactory.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Text;
 using OpenQA.Selenium;
-//using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 
 namespace Vaquinha.AutomatedUITests
@@ -14,34 +14,35 @@ namespace Vaquinha.AutomatedUITests
         public DriverFactory()
         {
             // Inicializa o browser utilizando o ChromeDriver que está no caminho /usr/share/applications/
-            //ChromeDriverService service = ChromeDriverService.CreateDefaultService("/usr/share/applications/");
-            FirefoxDriverService service = FirefoxDriverService.CreateDefaultService("/usr/share/applications/");
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService("/usr/bin");
+            // FirefoxDriverService service = FirefoxDriverService.CreateDefaultService("/usr/bin");
             
             // Faz criação de porta para abrir o browser.
             service.Port = new Random().Next(64000, 64800);
             
-            /*ChromeOptions options = new ChromeOptions();
+            ChromeOptions options = new ChromeOptions();
             options.AddArgument("headless");
             options.AddArgument("no-sandbox");
-            //options.AddArgument("proxy-server='direct://'");
+            options.AddArgument("proxy-server='direct://'");
             options.AddArgument("proxy-auto-detect");
             options.AddArgument("proxy-bypass-list=*");
             options.AddUserProfilePreference("disable-popup-blocking", "true");
-*/
+
             // Inicializa o IWebDriver do selenium, é ele que disponibiliza as consultas e manipulacoes das paginas. 
-            //_driver = new ChromeDriver(service, options);
+            _driver = new ChromeDriver(service, options);
             CodePagesEncodingProvider.Instance.GetEncoding(437);
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            FirefoxOptions options = new FirefoxOptions();
+            //FirefoxOptions options = new FirefoxOptions();
             options.AddArgument("-headless");
             options.AddArgument("-safe-mode");
             options.AddArgument("-ignore-certificate-errors");
+            /*
             FirefoxProfile profile = new FirefoxProfile();
             profile.AcceptUntrustedCertificates = true;
             profile.AssumeUntrustedCertificateIssuer = false;
-            options.Profile = profile;
-            
-            _driver = new FirefoxDriver(service, options);
+            //options.Profile = profile;
+            */
+            //_driver = new FirefoxDriver(service, options);
             
             _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
             _driver.Manage().Window.Maximize();
